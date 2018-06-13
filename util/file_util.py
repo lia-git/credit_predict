65ac051b829d1f8874382c20e4267ca344df46b8
lia-git <liaharward@gmail.com>
2018-06-13 11:08:26 +0800

    update

--- util/file_util.py
+++ util/file_util.py
@@ -18,10 +18,11 @@ logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 logger.setLevel(logging.INFO)
 
-def load_csv(file_name):
+def load_raw_csv(file_name):
     data_frame = pd.read_csv(file_name,header = 0)
     logger.info(" {} features :{}".format(len(data_frame.columns),data_frame.columns.tolist()))
     delete_cols = []
+    left_cols = []
     float_value_cols = []
     cat_cols = []
     for col in data_frame.columns:
@@ -49,6 +50,8 @@ def load_csv(file_name):
                     float_value_cols.append(col)
                     type= "continues"
         cx= pd_col.dtypes
+        if pd_col.isnull().sum()/len(pd_col) ==1:
+            left_cols.append(col)
         if pd_col.isnull().sum() >0:
             logger.warning("Raw,name:{}-type:{}-uni_count:{}-nan_count:{}-ratos:{}".format(col,type,len(uni_value),
                                                                                            pd_col.isnull().sum(),
@@ -57,12 +60,19 @@ def load_csv(file_name):
     logger.warning("size = {},delete_cols {}".format(len(delete_cols),delete_cols))
     logger.warning("size = {},cat_cols :{}".format(len(cat_cols),cat_cols))
     logger.warning("size = {},float_cols :{}".format(len(float_value_cols),float_value_cols))
+    return data_frame[left_cols]
     # logger.info(data_frame.head(5))
+
+def save_file(df,file_name):
+    df.to_csv(file_name,index=False)
+
 
 
 
 
 if __name__ == '__main__':
-    load_csv("../data/application_train.csv")
+    force_data = load_raw_csv("../data/application_train.csv")
+    logger.info(force_data.head(5))
+    save_file(force_data,"../data/application_train_forced.csv")
 
 
